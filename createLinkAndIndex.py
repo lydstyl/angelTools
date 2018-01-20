b2fbf4bc2d371444ec20ae95f9b09d1422f34489
Gabriel <lydstyl@gmail.com>
2018-01-20 19:57:15 +0100

    script python pour créer les lien de la home angel tools

---
+++ createLinkAndIndex.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3.5
+# vim : set fileencoding=utf-8 :
+# script créé par LYD
+
+"""
+Permet de générer les liens pour le index.html de angel-tools.ddns.net
+"""
+import os
+
+fileNames = os.listdir('.')
+exeptions = ['.git', 'PrintX','ajouter ici les folders à ne pas linker']
+
+html_str = """
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Angel Tools</title>
+</head>
+<body>
+
+<a href="http://angel-tools.ddns.net:8080/">nodeServer</a>
+"""
+
+for fileName in fileNames:
+    if os.path.isdir('./' + fileName) and fileName not in exeptions:
+        html_str += '<a href="http://angel-tools.ddns.net/' + fileName + '">' + fileName + '</a>'
+
+html_str += "</body></html>"
+
+Html_file= open("index.html","w")
+Html_file.write(html_str)
+Html_file.close()
